b5bbf74dd1f864c926af26bdcec589ca5269f272
Luis Parrado <luisprmat@gmail.com>
2025-05-24 10:00:34 -0500

    Create models, migrations, relationships, factory, seeders and seed DB
    Add index to companies table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Company::factory(1000)->create()->each(fn (Company $company) => $company->users()
+            ->createMany(User::factory(50)->make()->map->getAttributes()));
     }
 }
