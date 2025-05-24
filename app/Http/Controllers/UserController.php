771acc9c8857f08006cd2cd6ba1cfabd7a8c103d
Luis Parrado <luisprmat@gmail.com>
2025-05-24 13:24:40 -0500

    Create views and controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\View\View;
+
+class UserController extends Controller
+{
+    public function index(): View
+    {
+        $users = User::query()
+            ->paginate();
+
+        return view('users.index', ['users' => $users]);
+    }
+}
