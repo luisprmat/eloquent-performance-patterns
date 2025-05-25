ba952c5f35278ca8edd9fd64797eab66c82b0c0c
Luis Parrado <luisprmat@gmail.com>
2025-05-25 18:07:39 -0500

    Create models, migrations, relationships, factory, seeders and seed DB. Use Enums

---
+++ app/Enums/FeatureActionEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+enum FeatureActionEnum: string
+{
+    case ADD = 'Add';
+    case FIX = 'Fix';
+    case IMPROVE = 'Improve';
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Feature;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(250)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Feature::factory()->count(60)->create()->each(function (Feature $feature) use ($users) {
+            $feature->comments()->createMany(
+                Comment::factory()->count(rand(1, 50))->make()->each(function (Comment $comment) use ($users) {
+                    $comment->user_id = $users->random()->id;
+                })->toArray()
+            );
+        });
     }
 }
