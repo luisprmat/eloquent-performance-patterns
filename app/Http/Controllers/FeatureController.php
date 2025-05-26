6cf50686e0eb647df89650de6165640a34f9e135
Luis Parrado <luisprmat@gmail.com>
2025-05-26 01:20:53 -0500

    L05-Use filter clause if database is postgres

--- app/Http/Controllers/FeatureController.php
+++ app/Http/Controllers/FeatureController.php
@@ -8,11 +8,21 @@ class FeatureController extends Controller
 {
     public function index()
     {
-        $statuses = Feature::toBase()
-            ->selectRaw("count(case when status = 'Requested' then 1 end) as requested")
-            ->selectRaw("count(case when status = 'Planned' then 1 end) as planned")
-            ->selectRaw("count(case when status = 'Completed' then 1 end) as completed")
-            ->first();
+        if (config('database.default') === 'mysql' || config('database.default') === 'sqlite') {
+            $statuses = Feature::toBase()
+                ->selectRaw("count(case when status = 'Requested' then 1 end) as requested")
+                ->selectRaw("count(case when status = 'Planned' then 1 end) as planned")
+                ->selectRaw("count(case when status = 'Completed' then 1 end) as completed")
+                ->first();
+        }
+
+        if (config('database.default') === 'pgsql') {
+            $statuses = Feature::toBase()
+                ->selectRaw("count(*) filter (where status = 'Requested') as requested")
+                ->selectRaw("count(*) filter (where status = 'Planned') as planned")
+                ->selectRaw("count(*) filter (where status = 'Completed') as completed")
+                ->first();
+        }
 
         $features = Feature::query()
             ->withCount('comments')
