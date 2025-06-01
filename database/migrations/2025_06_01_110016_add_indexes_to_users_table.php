3de437499b469af2ffec12de2e1c7aefbfb35230
Luis Parrado <luisprmat@gmail.com>
2025-06-01 11:22:36 -0500

    L14-Solution - Add compound index (column order is important)

--- database/migrations/2025_06_01_110016_add_indexes_to_users_table.php
+++ database/migrations/2025_06_01_110016_add_indexes_to_users_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->index('first_name');
-            $table->index('last_name');
+            $table->index(['last_name', 'first_name']);
         });
     }
 
@@ -23,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropIndex(['last_name']);
-            $table->dropIndex(['first_name']);
+            $table->dropIndex(['last_name', 'first_name']);
         });
     }
 };
