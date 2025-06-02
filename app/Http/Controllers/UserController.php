542b1f4afeee1fabfe4cc68b6a00c6b964f7be83
Luis Parrado <luisprmat@gmail.com>
2025-06-01 21:47:57 -0500

    Modify models, migrations, factories, seeders, views and controller.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,8 @@ class UserController extends Controller
     public function index(): View
     {
         $users = User::query()
+            ->orderBy('name')
+            ->with('company')
             ->paginate();
 
         return view('users.index', ['users' => $users]);
