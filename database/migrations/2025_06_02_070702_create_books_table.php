0013b953c284e78669701ddd9a4ed839d04c4789
Luis Parrado <luisprmat@gmail.com>
2025-06-02 09:20:47 -0500

    L18-Caching last checkout (improve performance if large data)

--- database/migrations/2025_06_02_070702_create_books_table.php
+++ database/migrations/2025_06_02_070702_create_books_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('last_checkout_id')->nullable()->constrained('checkouts');
             $table->string('name');
             $table->string('author');
             $table->timestamps();
