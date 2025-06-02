500022bd6b1a5896d219d70b725a83c624f6bb72
Luis Parrado <luisprmat@gmail.com>
2025-06-02 08:50:22 -0500

    L18-Order by last checkout using join

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,6 +10,10 @@ class BookController extends Controller
     public function index(): View
     {
         $books = Book::query()
+            ->select('books.*')
+            ->join('checkouts', 'checkouts.book_id', '=', 'books.id')
+            ->groupBy('books.id')
+            ->orderByRaw('max(checkouts.borrowed_date) desc')
             ->withLastCheckout()
             ->with('lastCheckout.user')
             ->paginate();
