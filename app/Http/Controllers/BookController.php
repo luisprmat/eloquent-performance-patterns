80f79eb3360d5ad800186cd1f95491e25fa4830c
Luis Parrado <luisprmat@gmail.com>
2025-06-02 09:05:13 -0500

    L18-Order using subquery without Checkout model pivot

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
-use App\Models\Checkout;
+use Illuminate\Database\Query\Builder;
 use Illuminate\View\View;
 
 class BookController extends Controller
@@ -11,11 +11,13 @@ class BookController extends Controller
     public function index(): View
     {
         $books = Book::query()
-            ->orderByDesc(Checkout::select('borrowed_date')
-                ->whereColumn('book_id', 'books.id')
-                ->latest('borrowed_date')
-                ->take(1)
-            )
+            ->orderByDesc(function (Builder $query) {
+                $query->select('borrowed_date')
+                    ->from('checkouts')
+                    ->whereColumn('book_id', 'books.id')
+                    ->latest('borrowed_date')
+                    ->take(1);
+            })
             ->withLastCheckout()
             ->with('lastCheckout.user')
             ->paginate();
