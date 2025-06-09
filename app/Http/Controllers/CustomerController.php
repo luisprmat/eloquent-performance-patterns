f5776e28843dc4de2b94a56420d7c23d1388791e
Luis Parrado <luisprmat@gmail.com>
2025-06-08 21:18:27 -0500

    L28-Filter Customers By Region

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -10,9 +10,11 @@ class CustomerController extends Controller
 {
     public function index(): View
     {
+        // To find the customers for a specific region:
         $regions = Region::all();
 
         $customers = Customer::query()
+            ->inRegion(Region::where('name', 'The Prairies')->first())
             ->get();
 
         return view('customers.index', [
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QBuilder;
 
 class Customer extends Model
 {
@@ -29,5 +31,21 @@ class Customer extends Model
                 $query->selectRaw('ST_Y(location::geometry) as latitude, ST_X(location::geometry) as longitude');
             }
         });
+    }
+
+    public function scopeInRegion(Builder $query, Region $region): void
+    {
+        /** @var QBuilder $query */
+        if (config('database.default') === 'mysql') {
+            $query->whereRaw('ST_Contains(?, customers.location)', [$region->geometry]);
+        }
+
+        if (config('database.default') === 'sqlite') {
+            throw new \Exception('This lesson does not support SQLite.');
+        }
+
+        if (config('database.default') === 'pgsql') {
+            $query->whereRaw('ST_Contains(?, customers.location::geometry)', [$region->geometry]);
+        }
     }
 }
